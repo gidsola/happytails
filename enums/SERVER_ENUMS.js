45136ccdccca49676e3ee1ddbee3dea92ec8fe44
Goodsie <gidsola@users.noreply.github.com>
2026-02-18 14:56:25 -0500

    remove logging and test Literal output on cloud logs.

--- enums/SERVER_ENUMS.js
+++ enums/SERVER_ENUMS.js
@@ -11,9 +11,9 @@ const __dirname = resolve();
  * @returns {string} The absolute path to index.html
  */
 const INDEX_HTML = () => join(__dirname, 'frontend', 'dist', 'index.html');
-console.log('Index HTML path:', INDEX_HTML());
+// console.log('Index HTML path:', INDEX_HTML());
 
 const STATIC_ASSETS = () => join(__dirname, 'frontend', 'dist');
-console.log('Static assets path:', STATIC_ASSETS());
+// console.log('Static assets path:', STATIC_ASSETS());
 
 export { INDEX_HTML, STATIC_ASSETS };
